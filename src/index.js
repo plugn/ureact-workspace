bee39f3a610fd27bf425bda575cf5fef9a0d751f
Jonathan Reem <jonathan.reem@gmail.com>
2017-04-03 13:48:38 -0700

    fix(all): Actually export all needed values.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 export {default as Workspace} from './workspace/index';
 export {default as WorkspaceEditor} from './workspace-editor/index';
+
+export {
+  default as workspaceEditorApp,
+  saga as workspaceEditorSaga,
+  initialState as workspaceEditorInitialState,
+  getProject, setProject
+} from './workspace-editor/updater';
+
+export {
+  default as masterApp,
+  saga as masterSaga,
+  initialState as masterInitialState,
+  getState as getMasterState,
+  supply as supplyMaster,
+} from './masters/updater';
+
+export {default as Project} from './models/project';
